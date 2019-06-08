ae3a0ae8d7ef3d23e915447afa96d506d527c74e
HarmanRayat <harmanrayat@gmail.com>
2019-06-08 09:25:56 -0700

    Saturday - FAQs (6 views) (8th June)

---
+++ Puja Locator/Scripts Swift/Q1.swift
@@ -0,0 +1,32 @@
+//
+//  Q1.swift
+//  Puja Locator
+//
+//  Created by Harmanjyot on 08/06/19.
+//  Copyright © 2019 Harmanjyot. All rights reserved.
+//
+
+import UIKit
+
+class Q1: UIViewController {
+
+    @IBOutlet weak var Q1Lbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Q1Lbl.adjustsFontForContentSizeCategory = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
