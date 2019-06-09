9bd8e589d56c444ca4e344f5a0e3f53fdc567973
HarmanRayat <harmanrayat@gmail.com>
2019-06-09 01:52:45 -0700

    Sundayyyyyyy
    Life goes on

---
+++ Puja Locator/Scripts Swift/scrollFAQs.swift
@@ -0,0 +1,31 @@
+//
+//  scrollFAQs.swift
+//  Puja Locator
+//
+//  Created by Harmanjyot on 09/06/19.
+//  Copyright © 2019 Harmanjyot. All rights reserved.
+//
+
+import UIKit
+
+class scrollFAQs: UIViewController {
+
+    @IBOutlet weak var topHead: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
